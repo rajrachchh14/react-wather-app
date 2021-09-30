c39d40a546e0f8fe3abedc53ec648d2b69ba7ebf
rajrachchh14 <rajrachchh14@gmail.com>
2021-09-30 18:52:45 +0530

    1 file modified

--- src/App.js
+++ src/App.js
@@ -4,15 +4,17 @@ export default function App() {
   let [formData, setformData] = useState('');
   let [responseCity, SetResponseCity] = useState('');
   let [responseTemp, SetResponseTemp] = useState('');
+  let [responseError, SetresponseError] = useState('');
 
   const FetchData = async () => {
     let url = `
     https://api.openweathermap.org/data/2.5/weather?q=${formData}&appid=e9207e1292e995798b33b5dcbf08a221`;
     const response = await fetch(url);
     const output = await response.json();
-    console.log(output);
-    SetResponseCity(output.name);
+    // console.log(output.cod);
+    SetResponseCity(output.name); // name
     SetResponseTemp(output.main); // Data get
+    SetresponseError(output.cod); // response err
   };
 
   const Input = (e) => {
@@ -26,6 +28,7 @@ export default function App() {
 
   return (
     <div>
+      {console.log(typeof responseError)}
       <section className="vh-100" style={{ BackgroundColor: '#f5f6f7' }}>
         <div className="container py-5 h-100">
           <div className="row d-flex justify-content-center align-items-center h-100">
@@ -45,39 +48,49 @@ export default function App() {
               >
                 Search
               </button>
-
               <br />
               <br />
-              <div
-                className="card bg-dark text-white"
-                style={{ BorderRadius: 40 }}
-              >
-                <div className="bg-image" style={{ BorderRadius: 35 }}>
-                  <img
-                    src="https://mdbootstrap.com/img/Photos/new-templates/bootstrap-weather/draw1.png"
-                    className="card-img"
-                    alt="weather"
-                  />
-                  <div
-                    className="mask"
-                    style={{ BackgroundColor: 'rgba(190, 216, 232,5)' }}
-                  ></div>
-                </div>
 
-                {!responseTemp.temp_min ? (
-                  'no response'
-                ) : (
+              {/* {responseError == 404 ? 'err' : 'true'} */}
+              {responseError == 404 ? (
+                'err'
+              ) : (
+                <div
+                  className="card bg-dark text-white"
+                  style={{ BorderRadius: 40 }}
+                >
+                  <div className="bg-image" style={{ BorderRadius: 35 }}>
+                    <img
+                      src="https://mdbootstrap.com/img/Photos/new-templates/bootstrap-weather/draw1.png"
+                      className="card-img"
+                      alt="weather"
+                    />
+                    <div
+                      className="mask"
+                      style={{ BackgroundColor: 'rgba(190, 216, 232,5)' }}
+                    ></div>
+                  </div>
+
                   <div className="card-img-overlay text-dark p-5">
                     <h4 className="mb-0">{responseCity ? responseCity : ''}</h4>
                     <p className="display-2 my-3"></p>
                     <p className="mb-2">
-                      Feels Like: <strong> Min {responseTemp.temp_min}</strong>
+                      Feels Like:
+                      <strong>
+                        Min
+                        {/* {responseTemp.temp_min} */}
+                        {responseError == 404 ? 'err' : responseTemp.temp_min}
+                      </strong>
                       &nbsp; &nbsp;
-                      <strong> Max {responseTemp.temp_max}</strong>
+                      <strong>
+                        Max
+                        {/* {responseTemp.temp_max} */}
+                        {responseError == 404 ? 'err' : responseTemp.temp_max}
+                      </strong>
                     </p>
                   </div>
-                )}
-              </div>
+                </div>
+              )}
             </div>
           </div>
         </div>
