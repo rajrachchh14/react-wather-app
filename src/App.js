9c5f3571ba63053b40d8cc378725277b9449255c
rajrachchh14 <rajrachchh14@gmail.com>
2021-09-30 21:30:23 +0530

    1 file modified

--- src/App.js
+++ src/App.js
@@ -1,46 +1,26 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 export default function App() {
-  let [formData, setformData] = useState('');
-  let [responseCity, SetResponseCity] = useState('');
-  let [responseTemp, SetResponseTemp] = useState('');
-  let [responseError, SetresponseError] = useState('');
+  let [search, setSearch] = useState('rajkot');
+  let [city, setCity] = useState(null);
 
-  const FetchData = async () => {
-    /*
-    let url = `
-    https://api.openweathermap.org/data/2.5/weather?q=${formData}&appid=e9207e1292e995798b33b5dcbf08a221`;
-    const response = await fetch(url);
-    const output = await response.json();
-    console.log(output.cod);
-    SetResponseCity(output.name); // name
-    SetResponseTemp(output.main); // Data get
-    */
-
-    let url = `
-      https://api.openweathermap.org/data/2.5/weather?q=${formData}&appid=e9207e1292e995798b33b5dcbf08a221`;
-    const response = await fetch(url);
-    const output = await response.json();
-    SetResponseCity(output.name); // name
-    SetResponseTemp(output.main); // Data get
-
-    // console.log(error);
-
-    SetResponseTemp(output.main); // Data get
-  };
+  useEffect(() => {
+    const FetchData = async () => {
+      let url = `
+    https://api.openweathermap.org/data/2.5/weather?q=${search}&appid=e9207e1292e995798b33b5dcbf08a221`;
+      const response = await fetch(url);
+      const output = await response.json();
+      setCity(output.main); // temp
+    };
+    FetchData();
+  }, [search]);
 
   const Input = (e) => {
-    setformData(e.target.value);
-  };
-
-  const Submit = () => {
-    // console.log('click');
-    FetchData();
+    setSearch(e.target.value);
   };
 
   return (
     <div>
-      {/* {console.log(responseError.length)} */}
       {/* {console.log(typeof responseError)} */}
       <section className="vh-100" style={{ BackgroundColor: '#f5f6f7' }}>
         <div className="container py-5 h-100">
@@ -48,24 +28,37 @@ export default function App() {
             <div className="col-md-10 col-lg-8 col-xl-6">
               <input
                 type="text"
-                value={formData}
+                value={search}
                 className="form-control"
                 placeholder="Search City"
                 onChange={Input}
               />
               <br />
-              <button
-                type="submit"
-                className="btn btn-success"
-                onClick={Submit}
-              >
-                Search
-              </button>
+
               <br />
               <br />
 
-              {/* {responseError == 404 ? 'err' : 'true'} */}
-              {responseError.toString() === 404 ? 'err' : responseTemp.temp_min}
+              {!city ? (
+                <div className="card" style={{ width: 300 }}>
+                  <div className="card-body text-center">
+                    <h5 className="card-title">City Not Found</h5>
+                    <p className="card-text">&nbsp;</p>
+                    <p className="card-text">&nbsp;</p>
+                  </div>
+                </div>
+              ) : (
+                <div className="card" style={{ width: 300 }}>
+                  <div className="card-body text-center">
+                    <h5 className="card-title">{search}</h5>
+                    <p className="card-text">
+                      Min Temprature : {city.temp_min}
+                    </p>
+                    <p className="card-text">
+                      Max Temprature : {city.temp_max}
+                    </p>
+                  </div>
+                </div>
+              )}
             </div>
           </div>
         </div>
